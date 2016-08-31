9e4dece11402c1910e6aec8fbb46517c956adac4
Luc Princen <luc@chefduweb.nl>
2016-08-31 12:52:02 +0200

    Version bump

--- stupid-llama.php
+++ stupid-llama.php
@@ -3,7 +3,7 @@
  * Plugin Name: Stupid Llama
  * Plugin URI: http://alpack.it/wordpress-tester-plugin
  * Description: Dumps a photo of a stupit llama in your stupid admin.
- * Version: 0.3.0
+ * Version: 0.3.1
  * Author: Luc Princen
  * Author URI: http://www.alpack.it
  * License: GPLv3
